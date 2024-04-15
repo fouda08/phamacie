8e40cb27dd44abe6bda0125a5bd4d2ba926b9b75
fouda mbia <loictagne@gmail.com>
2024-04-15 12:24:33 +0200

    creation d'un fichier home.htlm,index.css et test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+let nom =prompt("entrez votre nom");
+let nombre = prompt("entre un nombre");
+//document.write(nombre);
+if(nombre>25)
+{
+    document.write(nom," desole vous avez depasse l'age");
+}
+  else{
+      document.write(nom,"\tbienvenue dans notre club");
+  }
+
