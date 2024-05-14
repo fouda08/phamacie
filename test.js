f8a5f0e3f8642d944edb9648feb73f1f98bb15f4
fouda mbia genevieve <foudambiagenevieve08@gmail.com>
2024-05-14 09:49:06 +0200

    ok

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
-let nom =console.log("votre nom");
-let nombre = console.log("votre age");
-//document.write(nombre);
-if(nombre>25)
-{
-    document.write(nom," desole vous avez depasse l'age");
-}
-  else{
-      document.write(nom,"\tbienvenue dans notre club");
-  }
+// let nom = prompt("entre ton nom");
+// let nombre=prompt ("entre ton nom");
+// //document.write(nombre);
+// if(nombre>25)
+// {
+//     document.write(nom," desole vous avez depasse l'age");
+// }
+//   else{
+//       document.write(nom,"\tbienvenue dans notre club");
+//   }
 
+function afficherValeurs(){
+    let formulaire = document.getElementById('formulaire')
+    
+}
